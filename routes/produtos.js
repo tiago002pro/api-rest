588b156b6d1397790698ce23d6a2cf21fe96bb56
tiago002pro <tiagobarbosa02@outlook.com>
2023-03-04 09:23:50 -0300

    Conectando a API ao Banco MySQL

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,50 +1,93 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/', function (req, res, next) {
-    res.status(200).send({
-        mensagem: 'Usando o GET dentro da rota de produtos'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            'SELECT * FROM produtos;',
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error}) }
+                res.status(200).send({response: resultado});
+            }
+        );
+    })
 });
 
 router.post('/', (req, res, next) => {
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    }
-
-    res.status(201).send({
-        mensagem: 'Usando o POST dentro da rota de produtos',
-        produtoCriado: produto
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            'INSERT INTO produtos (nome, preco) VALUES (?, ?);',
+            [req.body.nome, req.body.preco],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error}) }
+                res.status(201).send({
+                    mensagem: 'Produto inserido com sucesso',
+                    id_produto: resultado.insertId
+                });
+            }
+        )
+    })
 });
 
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id
-
-    if (id ==  'especial') {
-        res.status(200).send({
-            mensagem: 'Você descobriu o ID especial',
-            id: id
-        });
-    } else {
-        res.status(200).send({
-            mensagem: 'Você passou um ID',
-            id: id
-        });
-    }
+router.get('/:id_produto', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            'SELECT * FROM produtos WHERE idprodutos = ?;',
+            [req.params.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error}) }
+                res.status(200).send({response: resultado});
+            }
+        );
+    })
 });
 
 router.patch('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o PATCH dentro da rota de produtos'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            `UPDATE produtos
+                SET nome     = ?,
+                    preco    = ?
+            WHERE idprodutos = ?`,
+            [
+                req.body.nome, 
+                req.body.preco,
+                req.body.id_produto
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error}) }
+                res.status(202).send({
+                    mensagem: 'Produto alterado com sucesso'
+                });
+            }
+        )
+    })
 });
 
 router.delete('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o DELETE dentro da rota de produtos'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        conn.query(
+            `DELETE FROM produtos WHERE idprodutos = ?`,
+            [req.body.id_produto],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error}) }
+                res.status(202).send({
+                    mensagem: 'Produto removido com sucesso'
+                });
+            }
+        )
+    })
 });
 
 module.exports = router;
